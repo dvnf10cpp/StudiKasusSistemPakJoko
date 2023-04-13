fc0d584763b0aa464a4da17393fb2a7bf7de5448
devanfer02 <ubdevanferrel04@student.ub.ac.id>
2023-04-14 02:43:26 +0700

    belajar lagii

--- bin/CPP/App.cpp
+++ bin/CPP/App.cpp
@@ -8,20 +8,91 @@ using namespace std;
 void login();
 void menu();
 void tambah();
+void display();
+void initData();
+void displayLayanan();
+void displayAntrian();
+string repeat(const string &str, int n); 
+vector<Admin> listAdmin;
+vector<Antrian> listAntrian;
+vector<Layanan> listLayanan;
 
 int main(){
+    initData();
     login(); 
     return 0;
 }
 
 void login(){
+    cout << (repeat("=",40)) << endl;
+    cout << ("Selamat datang pada laman login terminal sederhana") << endl;
+    do {
+        string username,password;
+        cout << "Username : ";
+        cin >> username;
+        cout << "Password : ";
+        cin >> password;
+
+    } while(1); 
     menu();
 }
 
 void menu(){
-
+    char choice;
+    cout << (repeat("=",40)) << endl;
+    cout << ("Selamat datang pada aplikasi terminal sederhana") << endl;
+    cout << ("Silahkan pilih salah satu menu dibawah ini") << endl;
+    cout << ("1. Menampilkan semua antrian pada hari ini") << endl;
+    cout << ("2. Menambahkan antrian") << endl;
+    cout << ("3. Log out dari sistem") << endl;
+    cout << ("Masukkan pilihan anda : ");
+    cin >> choice;
+    switch(choice){
+        case '1':
+            display();
+            break;
+        case '2':
+            tambah();
+            break;
+        case '3':
+            login();
+            break;
+    }
 }
 
 void tambah(){
 
+}
+
+void display(){
+
+}
+
+void initData(){
+    //List admin
+    listAdmin.push_back(Admin("joko", "ini_password_joko"));
+    listAdmin.push_back(Admin("dengklek", "ini_password_dengklek"));
+
+    //List layanan
+    string namaLayanan[] = {"Ganti Oli","Operasi Mobil","Ketok Magic","Modifikasi Mobil Racing", "Modifikasi Mobil Elektrik"};
+    int harga[] = {500000,2000000,3000000,5000000,5500000}; 
+    for(int i = 0; i < 5; i++){
+        listLayanan.push_back(Layanan(namaLayanan[i], harga[i]));
+    }
+}
+
+void displayLayanan(){
+
+}
+
+void displayAntrian(){
+
+}
+
+string repeat(const string &str, int n){
+    string res = "";
+    for(int i = 0; i < n; i++){
+        res += str;
+    }
+    return res;
 }
